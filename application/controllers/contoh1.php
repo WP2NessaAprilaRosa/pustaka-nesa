7c2e7a98bd3f03111bffac0890d48535ad8ed945
Nessa <nessaaprila30@gmail.com>
2023-09-25 16:04:44 +0700

    pustaka-nesa

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hallo Gaiss<h1>";
+        echo "Nama Saya nessa";
+        echo "Hobi saya kayang sambil overtingking
+            nim 19220351";
+    }
+    
+}
