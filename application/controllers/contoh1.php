1413a2e59b471c56eb6b377d06b6d3c54afd65ae
Nessa <nessaaprila30@gmail.com>
2023-10-03 13:52:28 +0700

    menambahkan latihan2

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,8 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Hallo Gaiss<h1>";
-        echo "Nama Saya nessa";
-        echo "Hobi saya kayang sambil overtingking
+        echo "Nama Saya Nessa";
+        echo " Hobi saya kayang sambil overtingking
             nim 19220351";
     }
     
