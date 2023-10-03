b9f02f0affbcc9b408b945e6f87ae6faf19b20fe
Nessa <nessaaprila30@gmail.com>
2023-10-03 17:46:29 +0700

    folder latihan 2

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <<html>
     <head>
         <title> Latihan 1 </title>
@@ -9,16 +8,4 @@
             ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
             $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
         </body>
-=======
-<<html>
-    <head>
-        <title> Latihan 1 </title>
-        <body>
-            halo kawan.. yuk kita belajar menghitung utang...!!<br>
-            Nilai 1 = <?= $nilai1; ?>
-            Nilai 2 = <?= $nilai2; ?>
-            ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-            $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
-        </body>
->>>>>>> bbc8e1aede38d8a0232657797af5eaee0d59f1eb
 </html>
