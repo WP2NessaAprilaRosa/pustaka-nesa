ae559f90d011b1a7b81ea5f9234e745b7d62e64e
Nessa <nessaaprila30@gmail.com>
2023-10-14 22:22:32 +0700

    update latihan 4

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,36 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    public function cetak()
+    {
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah','required|min_length[3]',
+        array(
+        'required' => 'Kode Matakuliah Harus diisi',
+        'min_length' => 'Kode terlalu pendek'
+        ));
+    $this->form_validation->set_rules ('nama', 'Nama Matakuliah','required|min_length[3]', 
+    array(
+        'required' => 'Nama Matakuliah Harus diisi',
+        'min_length' => 'Nama terlalu pendek'
+    ));
+    $this->form_validation->set_rules ('sks', 'sks Matakuliah','required', 
+    array(
+        'required' => 'SKS Harus diisi',
+    ));
+
+    if ($this->form_validation->run() == FALSE) {
+        $this->load->view('view-form-matakuliah');
+    } else {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks') 
+        ];
+        $this->load->view('view-data-matakuliah', $data);
+    }
+  }
+}
